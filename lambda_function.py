ab0dbb3178826e0e29fbe273b6a936793949cae6
marcelomarques <marcelo.marques@lampejjo.com>
2022-05-30 21:20:38 -0300

    update

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,34 @@
 import pymssql
+import pandas as pd
+
+def puket_conn():
+
+    server='187.0.145.98'
+    port=1433
+    user='plmexpress'
+    password='Wrk2@21%!'
+    database='HDB_Puket_Custo'
+
+    print(database)
+
+    dbconn = pymssql.connect(server=server, user=user, password=password, database=database, port=port)
+
+    return dbconn
+
 
 def lambda_handler(event, content):
 
-    print('hello')
+    query = """
+    SELECT
+    M.MATERIAL 'integration_id'
+    WHERE M.GRUPO='TECIDO'
+    """
 
-    return 'ok'
+    print('reading database')
+    material_db = pd.read_sql_query(query,conn=puket_conn())
+
+    print(f'len_result : {len(material_db)}')
+
+    return f'len_result : {len(material_db)}'
+
+
